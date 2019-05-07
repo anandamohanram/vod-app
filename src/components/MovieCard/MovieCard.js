6600de2b028e45f1471bdf35bac0fa20b9636a1e
anandamohanram <anandamohanram@gmail.com>
2019-05-07 15:04:25 +1200

    initial commit

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from './node_modules/react'
+
+export class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        Movie card
+      </div>
+    )
+  }
+}
+
+export default MovieCard
---
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class MovieCarousel extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default MovieCarousel
+
