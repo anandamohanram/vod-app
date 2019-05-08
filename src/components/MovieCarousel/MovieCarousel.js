0dbc9a9b14cec66c092598dec0d326fe4c32f9d6
anandamohanram <anandamohanram@gmail.com>
2019-05-08 15:52:22 +1200

    Add tabindex and plaed list attributes to Carousel

--- src/components/MovieCarousel/MovieCarousel.js
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-import { MovieCard } from '..';
+import React, { Component } from "react";
+import { MovieCard } from "..";
+import styles from "./movieCarousel.module.css";
 
 export class MovieCarousel extends Component {
+
   render() {
     return (
-      <div>
-        {this.props.movieList.entries.map((movieItem) => {    
-        return <MovieCard movie = {movieItem} key={movieItem.id}/>
+      <div id={this.props.id} className={styles.movieCarousel_wrapper}>
+        {this.props.movieList.map((movieItem, index) => {
+          return (
+            <MovieCard
+              tabIndex={index}
+              movie={movieItem}
+              key={movieItem.id}
+              playedMovie={this.props.playedMovie}
+              small={this.props.small}
+            />
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default MovieCarousel
-
+export default MovieCarousel;
