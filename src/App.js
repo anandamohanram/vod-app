be65221e1c602b90ccf0c07a67161fd53e2d4408
anandamohanram <anandamohanram@gmail.com>
2019-05-08 15:49:55 +1200

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './router/routes';
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
+    <Header />
     <Routes />
     </div>
   );
